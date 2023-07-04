861a42d4b60f474927517f2a89dfb0c5274da0ac
pedrolsp2 <54607320+pedrolsp2@users.noreply.github.com>
2023-07-04 15:40:46 -0300

    filtros

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useLayoutEffect } from 'react';
-import { View, FlatList, SafeAreaView, ActivityIndicator } from 'react-native';
+import { View, FlatList, SafeAreaView, ActivityIndicator, TextInput, TouchableOpacity, Text } from 'react-native';
 import firebase from '../../Configs/firebaseconfig.js';
 import UsersList from '../Components/UsersList/index.js';
 import { querryId } from '../../utils/storage.js';
@@ -8,6 +8,9 @@ const Search = () => {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
   const [myUser, setMyUser] = useState('');
+  const [searchName, setSearchName] = useState('');
+  const [searchCity, setSearchCity] = useState('');
+  const [showFilter, setShowFilter] = useState(true);
 
   useLayoutEffect(() => {
     const fetchUserData = async () => {
@@ -22,7 +25,17 @@ const Search = () => {
 
     const fetchUsers = async () => {
       try {
-        const snapshot = await firebase.firestore().collection('users').where('idUser', '!=', myUser).get();
+        let query = firebase.firestore().collection('users');
+
+        if (searchName !== '') {
+          query = query.where('nome', '>=', searchName).where('nome', '<=', searchName + '\uf8ff');
+        }
+
+        if (searchCity !== '') {
+          query = query.where('cidade', '>=', searchCity).where('cidade', '<=', searchCity + '\uf8ff');
+        }
+
+        const snapshot = await query.get();
         const fetchedUsers = snapshot.docs.map(doc => doc.data());
         setUsers(fetchedUsers);
         setLoading(false);
@@ -31,20 +44,43 @@ const Search = () => {
         setLoading(false);
       }
     };
+
     fetchUsers();
-      
-  }, []);
+  }, [searchName, searchCity]);
+
+  const toggleFilter = () => {
+    setShowFilter(!showFilter);
+  };
 
   if (loading) {
     return (
       <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <ActivityIndicator size="large" color="#14AF6C"/>
+        <ActivityIndicator size="large" color="#14AF6C" />
       </SafeAreaView>
     );
   }
 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <SafeAreaView style={{ flex: 1, marginTop: '10%' }}>
+      {showFilter && (
+        <View style={{ padding: 10, backgroundColor: '#F5F5F5' }}>
+          <TextInput
+            style={{ backgroundColor: '#FFF', padding: 10, marginBottom: 10 }}
+            placeholder="Filtrar por nome"
+            value={searchName}
+            onChangeText={text => setSearchName(text)}
+          />
+          <TextInput
+            style={{ backgroundColor: '#FFF', padding: 10 }}
+            placeholder="Filtrar por cidade"
+            value={searchCity}
+            onChangeText={text => setSearchCity(text)}
+          />
+        </View>
+      )}
+      <TouchableOpacity onPress={toggleFilter} style={{ padding: 10 }}>
+        <Text>{showFilter ? 'Ocultar filtros' : 'Mostrar filtros'}</Text>
+      </TouchableOpacity>
       <FlatList
         data={users}
         keyExtractor={item => item.idUser}
